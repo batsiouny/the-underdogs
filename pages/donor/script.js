1db2fb91185ac7fe26e8e39b3c430877bbd62cc4
batsiouny <seifeldin51@gmail.com>
2024-05-08 21:29:04 +0300

    created Donor, added basic styling and functionality

---
+++ pages/donor/script.js
@@ -0,0 +1,39 @@
+// Function to open registration form modal
+function openRegistrationForm() {
+    alert("Redirecting to registration page...");
+    // You can add actual functionality here to navigate to the registration page
+}
+// Function to close registration form modal
+function closeRegistrationForm() {
+    document.getElementById('registrationForm').style.display = 'none';
+}
+
+// Function to simulate viewing donation requests
+function viewDonationRequests() {
+    alert("Redirecting to view donation requests page...");
+    // You can add actual functionality here to navigate to the donation requests page
+}
+
+// Function to simulate selecting donation items
+function selectDonationItems() {
+    alert("Redirecting to select donation items page...");
+    // You can add actual functionality here to navigate to the select donation items page
+}
+
+// Function to simulate scheduling pickup
+function schedulePickup() {
+    alert("Redirecting to schedule pickup page...");
+    // You can add actual functionality here to navigate to the schedule pickup page
+}
+
+// Function to simulate viewing estimated time of arrival
+function viewETA() {
+    alert("Redirecting to view estimated time of arrival page...");
+    // You can add actual functionality here to navigate to the view ETA page
+}
+
+// Function to simulate receiving a notification for driver arrival
+function receiveNotification() {
+    alert("You have received a notification: The driver has arrived for pickup/drop-off.");
+    // You can add actual functionality here to handle receiving the notification
+}
